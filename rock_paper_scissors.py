7f55b486879ad3e29b424ece19ffdd05d350a260
Steven Salzar <Stevenasalazarm04@gmail.com>
2025-10-14 22:53:27 -0500

    este commit no se usara

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,14 +1,14 @@
 import random
 
 choices = ['rock', 'paper', 'scissors']
-
+jose = "pacheco"
 print("Rock Paper Scissors Game!")
 player = input("Choose (rock/paper/scissors): ").lower()
 computer = random.choice(choices)
 
 print(f"You chose: {player}")
 print(f"Computer chose: {computer}")
-
+print(f"jose jose {jose}")
 if player == computer:
     print("It's a tie!")
 elif (player == 'rock' and computer == 'scissors') or \
